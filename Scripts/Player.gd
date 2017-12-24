b6278b413f09114751e34d8a4e8fe6a8a07ab7e1
clbhundley <clbhundley@gmail.com>
2017-12-24 16:41:02 -0500

    UPDATE 12-14-17

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,23 +1,14 @@
 extends Control
 
-# Everything here should probably go in a GLOBALS script.
 
 var money = 0
-var slaves = {}
-var slavelist = []
+var slave_dict = {}
+var slave_list = []
 var week = 0
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-
-func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	pass
 
 func next_week():
-	for slave in slaves.values():
+	for slave in slave_dict.values():
 		assignment_report(slave)
 	week += 1
 
